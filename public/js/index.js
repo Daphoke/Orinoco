65e5b24f5347983dfb5ae9f3b01d47a0dcf2b51f
Dévid Luxin <deeve_l@hotmail.fr>
2021-10-07 23:00:58 +0200

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,61 @@
+
+
+
+
+<ul class="container py-4 row mx-auto gx-4 gy-2">
+		<li class="presentation-product col-4">
+			<a href="pages/products.html?type=teddies&id=5be9c8541c9d440000665243">
+				<figure class="shadow p-3 rounded-3">	
+					<img src="./images/materials/teddy_1.jpg" alt="" title="">	
+					<figcaption>
+						<h3>Norbert</h3>
+						<p>29.00 €</p>
+					</figcaption>
+				</figure>	
+			</a>
+		</li>
+		<li class="presentation-product col-4">
+			<a href="./produit.html">
+				<figure class="shadow p-3 rounded-3">
+					<img src="./images/materials/teddy_2.jpg" alt="" title="">	
+					<figcaption>
+						<h3>Arnold</h3>
+						<p>39.00 €</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li class="presentation-product col-4">
+			<a href="./produit.html">
+				<figure class="shadow p-3 rounded-3">
+					<img src="./images/materials/teddy_3.jpg" alt="" title="">	
+					<figcaption>
+						<h3>Lenny and Carl</h3>
+						<p>59.00 €</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li class="presentation-product col-4">
+			<a href="./produit.html">
+				<figure class="shadow p-3 rounded-3">
+					<img src="./images/materials/teddy_4.jpg" alt="" title="">	
+					<figcaption>
+						<h3>Gustav</h3>
+						<p>45.00 €</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li class="presentation-product col-4">
+			<a href="./produit.html">
+				<figure class="shadow p-3 rounded-3">
+					<img src="./images/materials/teddy_5.jpg" alt="" title="">	
+					<figcaption>
+						<h3>Garfunkel</h3>
+						<p>55.00 €</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>		
+	</ul>
